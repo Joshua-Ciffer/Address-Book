7a2c0ee015085b7495960b1bf295b6898ba18cbe
Joshua Ciffer <33296499+Joshua-Ciffer@users.noreply.github.com>
2018-03-06 09:23:44 -0500

    Created AddressBook interface
    Wrote methods that are required to be implemented for the project.

--- src/src/addressBook/backend/AddressBook.java
+++ src/src/addressBook/backend/AddressBook.java
@@ -1,3 +1,54 @@
-public interface AddressBook {
+package src.addressBook.backend;
 
+/**
+ * This interface contains the methods that must be implemented in the address book project.
+ *
+ * @author Joshua Ciffer
+ * @version 03/06/2018
+ */
+interface AddressBook {
+  
+ /**
+  * Adds a new entry.
+  *
+  * @returns True - If the entry is successfully added to the address book.
+  */
+  boolean addEntry();
+  
+  /**
+   * Deletes the current entry.
+   *
+   * @returns The info from the deleted entry.
+   */
+  String deleteEntry();
+  
+  /**
+   * Modifies the current entry.
+   */
+  void modifyEntry();
+  
+  /**
+   * Determines if there are any duplicate entries.
+   *
+   * @returns True - If more than one of the given entry exists.
+   */
+  boolean duplicateEntry();
+  
+  /**
+   * Locates a specific entry in the address book.
+   *
+   * @param name - The user's name.
+   * @param number - The user's number.
+   * @param address - The user's address.
+   * @returns The Address object associated with that user.
+   */
+  Address findUser(String name, int number, String address);
+  
+  /**
+   * Prints out the information of the user with the given name.
+   *
+   * @param name - The user's name.
+   */
+  void printUsers(String name);
+  
 }
