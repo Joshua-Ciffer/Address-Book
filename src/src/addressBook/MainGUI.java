dafe322e142e65b04fc1bf50fc2be1c766d69143
Joshua <Joshua_Ciffer@Outlook.com>
2018-03-08 01:04:12 -0500

    Setup package for GUI.

---
+++ src/src/addressBook/MainGUI.java
@@ -0,0 +1,20 @@
+package src.addressBook;
+
+/**
+ * 
+ *
+ * @author Joshua
+ * @version 2018/03/08
+ */
+public class MainGUI {
+
+	/**
+	 * Main entry point.
+	 *
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/src/addressBook/gui/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains GUI component classes.
+ *
+ * @author Joshua
+ * @version 03/08/2018
+ */
+package src.addressBook.gui;
